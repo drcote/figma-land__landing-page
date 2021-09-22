3a9b075670b6e462f3ace021832e4adcb07cc9dc
dr_cote <merezhin@gmail.com>
2021-09-23 00:17:41 +0300

    Стрит структуру сайта

---
+++ gulpfile.js
@@ -0,0 +1,78 @@
+"use strict";
+
+let gulp = require("gulp");
+let plumber = require("gulp-plumber");
+let sourcemap = require("gulp-sourcemaps");
+let sass = require("gulp-sass")(require("node-sass"));
+let postcss = require("gulp-postcss");
+let autoprefixer = require("autoprefixer");
+let server = require("browser-sync").create();
+let imagemin = require("gulp-imagemin");
+let webp = require("gulp-webp");
+let remane = require("gulp-rename");
+let svgstore = require("gulp-svgstore");
+let posthtml = require("gulp-posthtml");
+let include = require("posthtml-include");
+
+gulp.task("css", function () {
+  return gulp.src("source/sass/style.scss")
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("source/css"))
+    .pipe(server.stream());
+});
+
+gulp.task("server", function () {
+  server.init({
+    server: "source/",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
+  gulp.watch("source/*.html").on("change", server.reload);
+});
+
+gulp.task("images", function () {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({ optimizationLevel: 3 }),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("webp", function () {
+  return gulp.src("source/img/**/*.{png,jpg}")
+    .pipe(webp({ quality: 90 }))
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("sprite", function () {
+  return gulp.src("source/img/icon-*.svg")
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(remane("sprite.svg"))
+    .pipe(gulp.dest("source/img"));
+});
+
+/* gulp.task("html", function() {
+  return gulp.src("source/*.html")
+  .pipe(posthtml([
+    include()
+  ]))
+  .pipe(gulp.dest("source"));
+}); */
+
+gulp.task("build", gulp.series("css", "sprite"));
+gulp.task("start", gulp.series("build", "server"));
+gulp.task("production", gulp.series("build", "images"));
